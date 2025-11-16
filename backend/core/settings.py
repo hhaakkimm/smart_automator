dc4292311ad3b3f13480f35ed7bdea7e74389962
hkm <hhaakkimm@gmail.com>
2025-11-16 23:39:05 +0500

    set up docker-compose and started api

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -47,6 +47,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "djangorestframework",
+
 ]
 
 ROOT_URLCONF = "core.urls"
@@ -74,8 +76,12 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "automator_db",
+        "USER": "automator_user",
+        "PASSWORD": "automator_pass",
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
 
